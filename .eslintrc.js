e4a07e3a24d43304b53fc2728d82d2a2e2967b6d
Marcos Borges <marcos.abb52@gmail.com>
2020-02-20 14:23:39 -0300

    feat(app): add lint config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'standard-react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}

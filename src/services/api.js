bda150e39ce4a866be364231023994a96e37aaff
Marcos Borges <marcos.abb52@gmail.com>
2020-02-20 22:27:41 -0300

    feat(services): add api service interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
 import axios from 'axios'
 
+import store from 'store'
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL
+})
+
+api.interceptors.request.use(config => {
+  const { token } = store.getState().auth
+
+  const headers = { ...config.headers }
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`
+  }
+
+  return { ...config, headers }
 })
 
 export default api

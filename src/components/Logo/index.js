35bbd05170594a4ae4920f3cdedcd996c77efd50
Marcos Borges <marcos.abb52@gmail.com>
2020-02-26 19:33:47 -0300

    style(logo): improve logo styles

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,23 +1,19 @@
 import React from 'react'
 import t from 'prop-types'
 
-import { Container, Anchor, Wrapper, Icon, Remote, Name } from './styled'
+import { Container, Anchor, Wrapper, Icon, Name } from './styled'
 
 export default function Logo ({ to }) {
   return (
     <Container>
       {to ? (
         <Anchor to={to}>
-          <Icon size={24}>
-            <Remote size={24} />
-          </Icon>
+          <Icon />
         </Anchor>
       ) : (
         <Wrapper>
-          <Icon size={40}>
-            <Remote size={40} />
-          </Icon>
-          <Name>Controlaê</Name>
+          <Icon />
+          <Name>Controlaê | Dashboard</Name>
         </Wrapper>
       )}
     </Container>
--- src/components/Logo/styled.js
+++ src/components/Logo/styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { MdSettingsRemote } from 'react-icons/md'
-import { theme, prop } from 'styled-tools'
+import { theme } from 'styled-tools'
 
 export const Container = styled.div``
 
-export const Anchor = styled(Link)``
+export const Anchor = styled(Link)`
+  display: flex;
+`
 
 export const Wrapper = styled.div`
   display: flex;
@@ -15,20 +17,15 @@ export const Wrapper = styled.div`
   margin-bottom: ${theme('spacing.lg')};
 `
 
-export const Icon = styled.div`
-  width: ${prop('size')}px;
-  height: ${prop('size')}px;
-`
-
-export const Remote = styled(MdSettingsRemote).attrs(({ size }) => ({
-  size
-}))`
-  color: ${theme('colors.blue')};
+export const Icon = styled(MdSettingsRemote).attrs({
+  size: 28
+})`
+  color: ${theme('colors.text')};
 `
 
 export const Name = styled.span`
-  margin-top: ${theme('spacing.sm')};
-  font-size: ${theme('font.sm')};
-  text-transform: uppercase;
+  padding-top: ${theme('spacing.sm')};
+  font-size: ${theme('font.xs')};
   letter-spacing: 0.125rem;
+  text-transform: uppercase;
 `
